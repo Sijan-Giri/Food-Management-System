2c70f96ed927aa1a00c7dd222da5efd472ea9145
Sijan-Giri <girisijan346@gmail.com>
2024-07-12 20:09:26 +0545

    Product fetching Implementation

--- controller/admin/product/productController.js
+++ controller/admin/product/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../../../model/productModel");
 
 exports.createProduct = async(req,res) => {
+   try {
     const file = req.file;
     let filePath;
     if(!file) {
@@ -26,4 +27,47 @@ exports.createProduct = async(req,res) => {
     res.status(200).json({
         message : "Product created sucessfully"
     })
+   } catch (error) {
+    res.status(500).json({
+        message : "Something went wrong"
+    })
+   }
+}
+
+exports.getProducts = async (req,res) => {
+    const products = await Product.find();
+    if(products.length == 0) {
+        res.status(400).json({
+            message : "Products not found",
+            products : []
+        })
+    }
+    else {
+        res.status(200).json({
+            message : "Products fetched successfully",
+            products
+        })
+    }
+}
+
+exports.getProduct = async (req,res) => {
+    const {id} = req.params
+    if(!id) {
+        return res.status(400).json({
+            message : "Please provide Id"
+        })
+    }
+    const product = await Product.findById(id);
+    if(product.length == 0) {
+        res.status(400).json({
+            message : "Product not found",
+            product : []
+        })
+    }
+    else {
+        res.status(200).json({
+            message : "Product fetched successfully",
+            product
+        })
+    }
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,4 +1,4 @@
-const { createProduct } = require("../controller/admin/product/productController");
+const { createProduct, getProducts, getProduct } = require("../controller/admin/product/productController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const toRestrict = require("../middleware/toRestrict");
 
@@ -6,7 +6,9 @@ const router = require("express").Router();
 const {multer , storage} = require("../middleware/multerConfig");
 const upload = multer({storage : storage})
 
-router.route("/createProduct").post(isAuthenticated ,toRestrict("admin"),upload.single("productImage"), createProduct);
+router.route("/createProduct").post(isAuthenticated ,toRestrict("admin"),upload.single("productImage"), createProduct)
+.get(getProducts)
 
+router.route("/getProduct/:id").get(getProduct);
 
 module.exports = router;
