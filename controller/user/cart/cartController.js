cd6404ccb5209b32fdc7c9cc0af1261fcab5ffee
Sijan-Giri <girisijan346@gmail.com>
2024-07-30 16:24:24 +0545

    Delete From Cart

--- controller/user/cart/cartController.js
+++ controller/user/cart/cartController.js
@@ -24,3 +24,30 @@ exports.addToCart = async(req,res) => {
         message : "Product added to cart"
     })
 }
+
+exports.getCart = async (req,res) => {
+    const userId = req.user.id;
+    const userData = await User.findById(userId).populate("cart")
+    const cartData  = userData.cart
+    res.status(200).json({
+        message : "Cart fetched successfully",
+        data : cartData
+    })
+}
+
+exports.deleteFromCart = async(req,res) => {
+    const userId = req.user.id;
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    if(!product) {
+        return res.status(400).json({
+            message : "Product not found with this id"
+        })
+    }
+    const userData = await User.findById(userId);
+    userData.cart = userData.cart.filter((productId) => productId != id);
+    await userData.save();
+    res.status(200).json({
+        message : "Item deleted successfully from cart"
+    })
+}
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,11 +1,13 @@
-const { addToCart } = require("../controller/user/cart/cartController");
+const { addToCart, getCart, deleteFromCart } = require("../controller/user/cart/cartController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const catchAsync = require("../services/catchAsync");
 
 const router = require("express").Router()
 
 
-router.route("/addCart/:id").post(isAuthenticated,catchAsync(addToCart))
+router.route("/addCart/:id").post(isAuthenticated,catchAsync(addToCart));
+router.route("/getCart").get(isAuthenticated,catchAsync(getCart));
+router.route("/deleteCart/:id").delete(isAuthenticated,catchAsync(deleteFromCart));
 
 
 module.exports = router;
