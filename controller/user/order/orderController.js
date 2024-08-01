597862fb79eb339869670bb9b20f6edb7980cd70
Sijan-Giri <girisijan346@gmail.com>
2024-08-01 18:47:10 +0545

    UpdateOrder, deleteOrder , cancelOrder

--- controller/user/order/orderController.js
+++ controller/user/order/orderController.js
@@ -38,3 +38,99 @@ exports.getMyOrders = async (req,res) => {
         orders
     })
 }
+
+exports.updateMyOrder = async(req,res) => {
+    const {id} = req.params;
+    const {shippingAddress , item} = req.body;
+    if(!id) {
+        return res.status(400).json({
+            message : "Please provide id"
+        })
+    }
+    if(!shippingAddress || !item || !item.length) {
+        return res.status(400).json({
+            message : "Please provide shippingAddress & item"
+        })
+    }
+    const orderExists = await Order.findById(id);
+    if(!orderExists) {
+        return res.status(400).json({
+            message : "Order not found with this id"
+        })
+    }
+    if(orderExists.orderStatus == "onTheWay") {
+        return res.status(400).json({
+            message : "You cannot update order while it is on the way"
+        })
+    }
+    const updatedOrder = await Order.findByIdAndUpdate(id,{
+        shippingAddress,
+        item
+    },{
+        new : true
+    })
+    res.status(200).json({
+        message : "Order updated successfully",
+        data : updatedOrder
+    })
+}
+
+exports.deleteOrder = async(req,res) => {
+    const {id} = req.params;
+    const userId = req.user.id;
+    if(!id) {
+        return res.status(400).json({
+            message : "Please provide id"
+        })
+    }
+    const orderExists = await Order.findById(id);
+    if(!orderExists) {
+        return res.status(400).json({
+            message : "Order with this id doesn't exists"
+        })
+    }
+    if(orderExists.user !== userId) {
+        return res.status(400).json({
+            message : "You don't have permission to delete this order"
+        })
+    }
+    await Order.findByIdAndDelete(id);
+    res.status(200).json({
+        message : "Order deleted successfully"
+    })
+}
+
+exports.cancelOrder = async(req,res) => {
+    const {id} = req.params;
+    const userId = req.user.id;
+    if(!id) {
+        return res.status(400).json({
+            message : "Please provide id"
+        })
+    }
+    const order = await Order.findById(id);
+    if(!order) {
+        return res.status(400).json({
+            message : "Order not found with this id"
+        })
+    }
+    if(order.user !== userId) {
+        return res.status(400).json({
+            message : "You don't have permission to cancel this order"
+        })
+    }
+    if(order.orderStatus !== "pending") {
+        return res.status(400).json({
+            message : "You can't cancel this order cuz it is not in pending"
+        })
+    }
+    const updatedOrder = await Order.findByIdAndUpdate(id,{
+        orderStatus : "cancelled"
+    },{
+        new : true
+    })
+    res.status(200).json({
+        message : "Order cancelled successfully",
+        data : updatedOrder
+    })
+}
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 const { getOrders } = require("../controller/admin/order/orderController");
-const { createOrder, getMyOrders } = require("../controller/user/order/orderController");
+const { createOrder, getMyOrders, updateMyOrder, deleteOrder, cancelOrder } = require("../controller/user/order/orderController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const toRestrict = require("../middleware/toRestrict");
 const catchAsync = require("../services/catchAsync");
@@ -9,5 +9,8 @@ const router = require("express").Router();
 router.route("/createOrder").post(isAuthenticated,catchAsync(createOrder));
 router.route("/getMyOrder").get(isAuthenticated,catchAsync(getMyOrders));
 router.route("/getOrders").get(isAuthenticated,toRestrict("admin"),catchAsync(getOrders));
+router.route("/updateMyOrder/:id").patch(isAuthenticated,catchAsync(updateMyOrder));
+router.route("/deleteOrder/:id").delete(isAuthenticated,catchAsync(deleteOrder));
+router.route("/cancelOrder/:id").patch(isAuthenticated,catchAsync(cancelOrder));
 
 module.exports = router;
