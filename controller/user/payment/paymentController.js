d40dfcc2d0905dded9ff585732033c88e165593f
Sijan-Giri <girisijan346@gmail.com>
2024-08-06 22:04:37 +0545

    VerifyPidx Implementation

--- controller/user/payment/paymentController.js
+++ controller/user/payment/paymentController.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios");
+const Order = require("../../../model/orderSchema");
 
 exports.initiateKhaltiPayment = async (req,res) => {
     const {orderId , amount} = req.body;
@@ -8,19 +9,36 @@ exports.initiateKhaltiPayment = async (req,res) => {
         })
     }
     const data = {
-        return_url : "http://localhost:2000",
+        return_url : "http://localhost:2000/success",
         purchase_order_id : orderId,
         amount : amount,
         website_url : "http://localhost:2000/",
         purchase_order_name : "orderName_" + orderId 
     }
     
-     const response = await axios.post('https://a.khalti.com/api/v2/',data,{
+     const response = await axios.post('https://a.khalti.com/api/v2/epayment/initiate/',data,{
         headers : {
+            'Content-Type': 'application/json',
             'Authorization' : 'key 31907215237e454a968ce3a8e48665ad'
         }
     })
-    
-    console.log(response)
+    console.log(response.data)
+    const order = await Order.findById(orderId);
+    order.paymentDetails.pidx = response.data.pidx;
+    await order.save();
+    res.redirect(response.data.payment_url)
 }
 
+exports.verifyPidx = async(req,res) => {
+    const pidx = req.query.pidx;
+    const response = await axios.post("https://a.khalti.com/api/v2/epayment/lookup/",{pidx},{
+        headers : {
+            "Authorization" : 'key 31907215237e454a968ce3a8e48665ad'
+        }
+    })
+    if(response.data.status === "Completed") {
+        res.redirect("http://localhost:2000")
+    }else {
+        res.redirect("http://localhost:2000/errorPage")
+    }
+}
